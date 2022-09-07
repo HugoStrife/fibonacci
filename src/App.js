3dd01334c3d72cb7fce71ceb115d19444309415e
Pablo Guerrero <pablo.gocv@gmail.com>
2022-09-07 19:12:01 +0100

    context papito

--- src/App.js
+++ src/App.js
@@ -1,34 +1,71 @@
 import "./App.css";
-import React from "react";
-import { fibonacci } from "./components/sequence";
-function App() {
-  const [number, setNumber] = React.useState();
-  const [sequence, setSequence] = React.useState([]);
-  const handleClick = (e) => {
-    e.preventDefault();
-    if (!number || number === 0) {
-      alert("You must enter a number!!");
-    } else {
-      setSequence(fibonacci(number));
-    }
-  };
 
+import React, { useContext, useState } from "react";
+
+import { fibonacci } from "./components/sequence";
+
+function App() {
   return (
     <div className="App">
       <h1>Simple Fibonacci Guesser</h1>
       <h2>Enter a number to get it's Fibonacci sequence</h2>
-      <form onSubmit={handleClick}>
-        <input
-          type="number"
-          min="1"
-          max="10"
-          onChange={(e) => setNumber(e.target.valueAsNumber)}
-        />
-        <button type="submit">Get the sequence!</button>
-      </form>
-      <h2>Result: {sequence.join(",")}</h2>
+      <SequenceProvider>
+        <Form />
+        <Result />
+      </SequenceProvider>
     </div>
   );
 }
 
 export default App;
+
+// Form.jsx
+const Form = () => {
+  const [number, setNumber] = React.useState();
+  const sequenceContext = useContext(SequenceContext);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!number || number === 0) {
+      alert("You must enter a number!!");
+    } else {
+      const seq = fibonacci(number);
+      sequenceContext.setSequence(seq);
+    }
+  };
+
+  return (
+    <form onSubmit={handleClick}>
+      <input
+        type="number"
+        min="1"
+        max="10"
+        onChange={(e) => setNumber(e.target.valueAsNumber)}
+      />
+      <button type="submit">Get the sequence!</button>
+    </form>
+  );
+};
+
+// Result.jsx
+const Result = () => {
+  const { sequence } = useContext(SequenceContext);
+
+  return <h2>Result: {sequence.join(",")}</h2>;
+};
+
+// Context
+const SequenceContext = React.createContext({
+  sequence: null,
+  setSequence: null,
+});
+
+const SequenceProvider = ({ children }) => {
+  const [sequence, setSequence] = useState();
+
+  return (
+    <SequenceContext.Provider value={(sequence, setSequence)}>
+      {children}
+    </SequenceContext.Provider>
+  );
+};
